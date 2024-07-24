bfe1eace01bd1be5ab4a1f5af00f702d7fe72991
Tiffany Chen <tiffanychen@ts-air-2.lan>
2024-07-23 22:43:44 -0400

    added a new file

---
+++ main.py
@@ -0,0 +1,43 @@
+def encode(password):
+    encoded = ""
+    for char in password:
+        encoded += str((int(char) + 3) % 10)
+    return encoded
+
+
+def decode(password):
+    decoded = ""
+    for char in password:
+        decoded += str((int(char) - 3) % 10)
+    return decoded
+
+
+def main():
+    encoded_password = ""
+    while True:
+        print("Menu\n"
+              "-------------\n"
+              "1. Encode\n"
+              "2. Decode\n"
+              "3. Quit")
+        print()
+        option = input("Please enter an option: ")
+
+        if option == "1":
+            password = input("Please enter the password to encode: ")
+            encoded_password = encode(password)
+            print("Your password has been encoded and stored!")
+            print()
+        elif option == "2":
+            decoded_password = decode(encoded_password)
+            print(f"The encoded password is {encoded_password}, and the original password is {decoded_password}.")
+            print()
+        elif option == "3":
+            break
+
+        else:
+            print("Invalid option, please try again.")
+
+
+if __name__ == "__main__":
+    main()
